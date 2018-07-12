e638c417f717d06aee4dd80203fa820923c6baf3
jverneaut <jverneaut@gmail.com>
2018-07-12 14:33:53 +0200

    Global styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { injectGlobal, ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    light: '#f7f8fb',
+    dark: '#0a1d42',
+    primary: '#6d91ff'
+  }
+}
+
+injectGlobal`
+  body {
+    background: #f7f8fb;
+    font-family: Lato;
+    font-size: 18px;
+    color: ${theme.colors.dark};
+  }
+`;
 
 const App = () => (
-  <div>Julien Verneaut pour vous servir</div>
+  <ThemeProvider theme={theme}>
+    <div>Julien Verneaut pour vous servir</div>
+  </ThemeProvider>
 );
 
 export default App;
